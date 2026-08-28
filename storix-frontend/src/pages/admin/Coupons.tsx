938aaafaf0b6586b634c697192d2be37c5ec232e
Dharshanthaigu <50871173+Dharshanthaigu@users.noreply.github.com>
2026-08-28 20:12:56 +0530

    Update Coupons.tsx

--- storix-frontend/src/pages/admin/Coupons.tsx
+++ storix-frontend/src/pages/admin/Coupons.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, type FormEvent } from "react";
+import { useEffect, useState} from "react";
 import toast from "react-hot-toast";
 import { couponApi } from "../../api/couponApi";
 import type { Coupon } from "../../types";
